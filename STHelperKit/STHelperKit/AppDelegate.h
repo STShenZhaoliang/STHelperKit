fd899219f187e413e6a18cb56903b2fb08289540
stshenzhaoliang <409178030@qq.com>
2016-07-28 10:01:53 +0800

    2016年07月28日10:01:42 添加podfile

---
+++ STHelperKit/STHelperKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STHelperKit
+//
+//  Created by 沈兆良 on 16/7/28.
+//  Copyright © 2016年 沈兆良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
