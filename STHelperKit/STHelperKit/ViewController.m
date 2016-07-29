2e796563f443b92bfbb9f446f82ae4cd24d04f4f
stshenzhaoliang <409178030@qq.com>
2016-07-29 09:45:58 +0800

    2016年07月29日09:45:23 使用xcode8 创建目录

--- STHelperKit/STHelperKit/ViewController.m
+++ STHelperKit/STHelperKit/ViewController.m
@@ -8,20 +8,41 @@
 
 #import "ViewController.h"
 
+#pragma mark - --- Controller 控制器 ---
+
+#pragma mark - --- View 视图 ---
+
+#pragma mark - --- Model 数据 ---
+
+#pragma mark - --- Tool 工具 ---
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface ViewController ()
 
 @end
 
+NS_ASSUME_NONNULL_END
+
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark - --- lift cycle 生命周期 ---
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.view.backgroundColor = [UIColor redColor];
+//    self.view.layer.borderColor = [UIColor redColor].CGColor;
+//    self.view.layer.borderWidth = 0.5;
 }
+#pragma mark - --- delegate 视图委托 ---
+
+#pragma mark - --- event response 事件相应 ---
+
+#pragma mark - --- private methods 私有方法 ---
+
+#pragma mark - --- setters 属性 ---
+
+#pragma mark - --- getters 属性 ---
 
 @end
